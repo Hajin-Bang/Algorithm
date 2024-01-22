b8e75b807d6912d1752463ea0560a5207c8215cb
Hajin-Bang <138538168+Hajin-Bang@users.noreply.github.com>
2024-01-22 10:10:48 +0900

    [level 2] Title: 전화번호 목록, Time: 483.10 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,16 @@
+function solution(phone_book) {
+    const phoneDict = {}
+    
+    phone_book.forEach(phoneNumber => {
+        phoneDict[phoneNumber] = true
+    })
+
+    for(const phoneNumber of phone_book) {
+        for(let i = 1 ; i < phoneNumber.length ; i ++) {
+            const curStr = phoneNumber.slice(0, i)
+            if(phoneDict[curStr]) return false
+        }
+    }
+    
+    return true
+}
