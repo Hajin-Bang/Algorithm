5d0f150952a43b76f78eabc577acf659b5d34214
Hajin-Bang <138538168+Hajin-Bang@users.noreply.github.com>
2024-04-30 09:39:09 +0900

    [level 2] Title: 올바른 괄호, Time: 26.78 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,13 +1,11 @@
-function solution(s) {
+function solution(s){
     let count = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === '(') {
-            count++;
-        } else {
-            count--;
-            if (count < 0) {
-                return false;
-            }
+    for (let i=0; i<s.length; i++) {
+        if (s[i] === "(") {
+            count++
+        } else count--;
+        if (count<0) {
+            return false;
         }
     }
     return count === 0;
