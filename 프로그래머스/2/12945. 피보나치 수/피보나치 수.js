70d737f5c8f6f42ec6bf32a6b2bf4a2fde1798d2
Hajin-Bang <138538168+Hajin-Bang@users.noreply.github.com>
2024-06-05 23:00:05 +0900

    [level 2] Title: 피보나치 수, Time: 1.70 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let a = 0, b = 1, temp;
+    for (let i=2; i<=n; i++) {
+        temp = (a + b) % 1234567;
+        a = b;
+        b = temp;
+    }
+    return n === 0 ? a : b;
+}
