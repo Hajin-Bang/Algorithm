59346418ec3c84093c1e423f94c25350d985e2d6
Hajin-Bang <138538168+Hajin-Bang@users.noreply.github.com>
2024-06-12 09:28:32 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,12 @@
+function solution(brown, yellow) {
+    for (let i = 1; i <= Math.sqrt(yellow); i++) {
+        if (yellow % i === 0) {
+            let width = yellow / i;
+            let length = i;
+            if ((width + 2) * (length + 2) === brown + yellow) {
+                return [width + 2, length + 2];
+            }
+        }
+    }
+    return [];
+}
