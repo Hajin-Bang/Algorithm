5e7de1a796463da8c7ef0e770add3319911231ce
Hajin-Bang <138538168+Hajin-Bang@users.noreply.github.com>
2024-09-06 13:27:53 +0900

    [Bronze II] Title: 할리갈리, Time: 92 ms, Memory: 39452 KB -BaekjoonHub

--- 백준/Bronze/27160. 할리갈리/할리갈리.py
+++ 백준/Bronze/27160. 할리갈리/할리갈리.py
@@ -1,19 +1,26 @@
-N = int(input())
+import sys
+
+input = sys.stdin.read
+data = input().splitlines()
+
+N = int(data[0])
 
 STRAWBERRY = BANANA = LIME = PLUM = 0
 
-for i in range(N):
-    S, X = input().split()
+for i in range(1, N + 1):
+    S, X = data[i].split()
     X = int(X)
-    if (S == "STRAWBERRY"):
+    if S == "STRAWBERRY":
         STRAWBERRY += X
-    elif (S == "BANANA"):
+    elif S == "BANANA":
         BANANA += X
-    elif (S == "LIME"):
+    elif S == "LIME":
         LIME += X
-    elif (S == "PLUM"):
+    elif S == "PLUM":
         PLUM += X
 
-if (STRAWBERRY == 5 or BANANA == 5 or LIME == 5 or PLUM == 5):
+# 한 번만 모든 과일의 개수를 비교하는 최적화
+if STRAWBERRY == 5 or BANANA == 5 or LIME == 5 or PLUM == 5:
     print("YES")
-else: print("NO")
+else:
+    print("NO")
