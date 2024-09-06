22ce8c60b45abb2099ffd03c8d7520dfd0b29c00
Hajin-Bang <138538168+Hajin-Bang@users.noreply.github.com>
2024-09-06 13:42:27 +0900

    [Bronze II] Title: 상수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -3,6 +3,4 @@ A, B = map(list, input().split())
 reversedA = "".join(map(str, reversed(A)))
 reversedB = "".join(map(str, reversed(B)))
 
-if (int(reversedA) > int(reversedB)):
-    print(reversedA)
-else: print(reversedB)
+print(max(int(reversedA), int(reversedB)))
