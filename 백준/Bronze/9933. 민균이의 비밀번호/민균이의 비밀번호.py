7d610582cd9b1c3830d3d8d66d2d7d8cd56f6644
Hajin-Bang <138538168+Hajin-Bang@users.noreply.github.com>
2024-09-27 11:16:16 +0900

    [Bronze I] Title: 민균이의 비밀번호, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.py
+++ 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.py
@@ -9,8 +9,6 @@ for i in range(N):
     words[word] = word[::-1] # 키:문자열 / 값:뒤집은 문자열 형태로 words에 저장
     if word[::-1] in words: # 뒤집은 문자가 words의 키에 있다면
         result = word
-    elif word == word[::-1]:
-        result = word
 
 len_result = len(result)
 print(len_result, result[len_result // 2] if len_result else " ")
