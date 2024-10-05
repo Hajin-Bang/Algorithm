1d415848d4a9e5d4e0f462af7697e9813db8dfb9
Hajin-Bang <138538168+Hajin-Bang@users.noreply.github.com>
2024-10-05 15:07:51 +0900

    [Silver II] Title: 나무 자르기, Time: 1844 ms, Memory: 150200 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -2,7 +2,7 @@ import sys
 
 # 톱의 최대 높이를 찾기 위한 함수
 def binary_search(arr, start, end):
-    result = 0 # 톱의 최적 높이를 저장하는 변수
+    optimal_height = 0
 
     while start <= end:
         mid = (start + end) // 2 # 중간값을 톱의 높이로 설정
@@ -13,20 +13,22 @@ def binary_search(arr, start, end):
             if mid < tree:
                 total += tree - mid
 
-        # 총 나무의 길이가 목표치 M보다 작으면 
-        if total < M:
-            end = mid - 1 # 톱의 높이를 낮춤
-        else: # 목표치보다 같거나 크면 (mid로 충분히 목표를 달성할 수 있다는 뜻)
-            result = mid
-            start = mid + 1 # 탐색의 범위를 높여본다.(톱의 높이를 높임)
-    return result
+        if total == M: # 나무의 총 길이가 목표길이와 같다면
+            return mid # 바로 높이를 반환
+        
+        elif total < M: # 나무의 총 길이가 목표 길이보다 작다면 
+            end = mid - 1 # 높이를 더 줄여야하므로 범위의 끝을 중간보다 1작게 설정. 
+
+        else: # 나무의 총 길이가 목표 길이보다 크다면
+            optimal_height = mid # 현재 높이가 제일 적합할 수 있으므로 우선 저장
+            start = mid + 1 # 높이를 높여봐야하므로 범위의 시작을 중간보다 1크게 설정
+
+    return optimal_height
 
     
 
 input = sys.stdin.readline
 N, M = map(int, input().split())
 tree_heights = list(map(int, input().split()))
-
-
 
 print(binary_search(tree_heights, 0, max(tree_heights)))
